d5bea30e069a4fb9e0cd203999f15ba9797fbae2
Xin Qi Liu <liux3@bxscience.edu>
2022-01-15 20:21:25 -0500

    Update Minimax.java

--- Minimax.java
+++ Minimax.java
@@ -1,5 +1,36 @@
 package tictactoe;
 
-public class Minimax {
+import java.util.ArrayList;
+import java.lang.Math;
 
+public class Minimax {
+	private Board board;
+	
+	public void minMax() {
+		for(int i=0; i<board.getSize(); ++i) {
+			for(int j=0; j<board.getSize(); ++j) {
+				
+			}
+		}
+	}
+	
+	
+	// Random 
+	public void random() {
+		ArrayList<Integer[]> coordinates = new ArrayList<Integer[]>();
+		for(int i=0; i<board.getSize(); ++i) for(int j=0; j<board.getSize(); ++j) {
+			if(board.getBoard()[i][j].equals(" ")) {
+				Integer[] coord = {i, j};
+				coordinates.add(coord);
+			}
+		}
+	    if(coordinates.size()!=0) {	
+			Integer[] randomPick = coordinates.get((int)(Math.random() * coordinates.size()));
+	    	board.getBoard()[randomPick[0]][randomPick[1]] = new String("O");
+	    }
+    }
+	
+	Minimax(Board board){
+		this.board = board;
+	}
 }
