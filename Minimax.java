0e8fe49d257867a614a7924db4ac1e12fa20ae37
Xin Qi Liu <liux3@bxscience.edu>
2022-01-17 14:06:27 -0500

    Update Minimax.java

--- Minimax.java
+++ Minimax.java
@@ -1,24 +1,27 @@
 package tictactoe;
-import java.util.ArrayList;
 import java.lang.Math;
 
 public class Minimax {
 	private Board board;
-	private int depth = 2;
 	
 	public int[] ai() {
-		int bestScore = Integer.MIN_VALUE;
+		int bestScore = Integer.MAX_VALUE;
 		int[] bestCoordinates = new int[2];
+		if(board.getBoard()[1][1].equals(" ")) {
+			int[] best = {1, 1};
+			return best;
+		}
 		for(int i=0; i<board.size(); ++i) {
 			for(int j=0; j<board.size(); ++j) {
 				if(this.board.getBoard()[i][j].equals(" ")) {
 					board.getBoard()[i][j] = new String("O");
 					int score = minMax(false);
 					board.getBoard()[i][j] = new String(" ");
-					if(score > bestScore) {
+					if(score < bestScore) {
 						bestScore = score;
 						bestCoordinates[0] = i;
 						bestCoordinates[1] = j;
+						System.out.println(bestScore);
 					}
 				}
 			}
@@ -26,19 +29,19 @@ public class Minimax {
 		return bestCoordinates;
 	}
 	
-	public int minMax(boolean minimize) {
+	public int minMax(boolean maximize) {
 		boolean xWin = board.checkWin(new String("X"));
 		boolean oWin = board.checkWin(new String("O"));
 		boolean draw = board.checkDraw();
 		if(xWin) return 1;
 		else if(oWin) return -1;
 		else if(draw) return 0;
-		if(minimize) {
+		if(maximize) {
 			int bestScore = Integer.MAX_VALUE;
 			for(int i=0; i<board.size(); ++i) for(int j=0; j<board.size(); ++j) {
 				if(board.getBoard()[i][j].equals(" ")) {
 					board.getBoard()[i][j] = new String("O");
-					int score = minMax(true);
+					int score = minMax(false);
 					board.getBoard()[i][j] = new String(" ");
 					bestScore = Math.min(score, bestScore);
 				}
@@ -49,7 +52,7 @@ public class Minimax {
 			for(int i=0; i<board.size(); ++i) for(int j=0; j<board.size(); ++j) {
 				if(board.getBoard()[i][j].equals(" ")) {
 					board.getBoard()[i][j] = new String("X");
-					int score = minMax(false);
+					int score = minMax(true);
 					board.getBoard()[i][j] = new String(" ");
 					bestScore = Math.max(score, bestScore);
 				}
